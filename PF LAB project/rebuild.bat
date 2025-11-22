9b86ede6ba57bc50ec840cd2c61e96038138e983
k250018-alt <k250018@nu.edu.pk>
2025-11-23 02:46:29 +0500

    Delete PF LAB project directory

--- PF LAB project/rebuild.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "C:\Users\User\Desktop\PF LAB project\build"
-C:\msys64\usr\bin\bash.exe -i -c "gcc -o car_game -I../include $(pkg-config --cflags gtk+-3.0) ../src/main.c ../src/game.c ../src/player.c ../src/obstacle.c ../src/graphics.c $(pkg-config --libs gtk+-3.0) -lm && echo SUCCESS"
